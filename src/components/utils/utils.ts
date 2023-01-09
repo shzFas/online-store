cdc6a190ea92dbbd37d1d8193d39ab23d43bdb62
shzfas <yuraka200486@gmail.com>
2023-01-09 10:45:11 +0600

    feat: main utils

---
+++ src/components/utils/utils.ts
@@ -0,0 +1,7 @@
+const createElement = (template: string): HTMLElement => {
+  const newElement = document.createElement('div');
+  newElement.innerHTML = template;
+  return newElement.firstElementChild as HTMLElement
+};
+
+export { createElement }
