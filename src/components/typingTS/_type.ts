4dc71149fc43c4759272879c9a4d69084cfdfb79
shzfas <yuraka200486@gmail.com>
2023-01-09 09:16:43 +0600

    feat: typingTS (типизация)

---
+++ src/components/typingTS/_type.ts
@@ -0,0 +1,20 @@
+
+
+type stringObject = {
+  [x: string]: string;
+}
+
+type numberObject = {
+  [x: string]: number;
+}
+
+type numberArrayObject = {
+  [x: string]: number[];
+}
+
+type stringArrayObject = {
+  [x: string]: string[];
+}
+
+
+export { stringObject, numberObject, numberArrayObject, stringArrayObject}
